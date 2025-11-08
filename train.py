5d7ef76572fb228c127f4f9f79349683a3aaabf8
codevalhalla <prsk3496@gmail.com>
2025-11-08 15:18:18 +0530

    edit train.py script

--- train.py
+++ train.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import pickle
+
 import pandas as pd
 import numpy as np
+
 import sklearn
-import pickle
+
 from sklearn.pipeline import make_pipeline
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LogisticRegression
@@ -14,25 +17,29 @@ print(f'numpy=={np.__version__}')
 print(f'sklearn=={sklearn.__version__}')
 
 
-df = pd.read_csv('./WA_Fn-UseC_-Telco-Customer-Churn.csv')
+def load_data():
+    df = pd.read_csv('./WA_Fn-UseC_-Telco-Customer-Churn.csv')
 
-df.columns = df.columns.str.lower().str.replace(' ', '_')
+    df.columns = df.columns.str.lower().str.replace(' ', '_')
 
-categorical_columns = list(df.dtypes[df.dtypes == 'object'].index)
+    categorical_columns = list(df.dtypes[df.dtypes == 'object'].index)
 
-for c in categorical_columns:
-    df[c] = df[c].str.lower().str.replace(' ', '_')
+    for c in categorical_columns:
+        df[c] = df[c].str.lower().str.replace(' ', '_')
 
-df.totalcharges = pd.to_numeric(df.totalcharges, errors='coerce')
-df.totalcharges = df.totalcharges.fillna(0)
+    df.totalcharges = pd.to_numeric(df.totalcharges, errors='coerce')
+    df.totalcharges = df.totalcharges.fillna(0)
 
-df.churn = (df.churn == 'yes').astype(int)
+    df.churn = (df.churn == 'yes').astype(int)
 
-y_train = df.churn
+    return df 
 
-numerical = ['tenure', 'monthlycharges', 'totalcharges']
 
-categorical = [
+    
+def train_model(df):
+    numerical = ['tenure', 'monthlycharges', 'totalcharges']
+    
+    categorical = [
     'gender',
     'seniorcitizen',
     'partner',
@@ -49,20 +56,26 @@ categorical = [
     'contract',
     'paperlessbilling',
     'paymentmethod',
-]
-
-
-pipeline = make_pipeline(
+    ]
+    
+    pipeline = make_pipeline(
     DictVectorizer(),
     LogisticRegression(solver='liblinear')
-)
+    )
+
+    train_dict = df[categorical + numerical].to_dict(orient='records')
+    y_train = df.churn
+    pipeline.fit(train_dict, y_train)
+    
+    return pipeline
+
+def save_model(pipeline, output_file):
+    with open(output_file, 'wb') as f_out:
+        pickle.dump(pipeline, f_out)
 
 
-train_dict = df[categorical + numerical].to_dict(orient='records')
+df = load_data()
+pipeline = train_model(df)
+save_model(pipeline,'model.bin')
 
-pipeline.fit(train_dict, y_train)
-
-
-with open('model.bin','wb') as f_out:
-    pickle.dump(pipeline,f_out)
-
+print('model saved to model.bin')

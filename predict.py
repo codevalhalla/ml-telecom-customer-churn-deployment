280da377a3208d26478554346da7dda032615c57
codevalhalla <prsk3496@gmail.com>
2025-11-08 19:04:36 +0530

    pydantic class for predictresponse

--- predict.py
+++ predict.py
@@ -3,11 +3,45 @@
 
 import pickle
 
-from typing import Dict, Any
+from typing import Dict, Any, Literal
 
 import uvicorn
 
 from fastapi import FastAPI
+
+from pydantic import BaseModel,Field
+
+    
+
+class Customer(BaseModel):
+    gender: Literal["male", "female"]
+    seniorcitizen: Literal[0, 1]
+    partner: Literal["yes", "no"]
+    dependents: Literal["yes", "no"]
+    phoneservice: Literal["yes", "no"]
+    multiplelines: Literal["no", "yes", "no_phone_service"]
+    internetservice: Literal["dsl", "fiber_optic", "no"]
+    onlinesecurity: Literal["no", "yes", "no_internet_service"]
+    onlinebackup: Literal["no", "yes", "no_internet_service"]
+    deviceprotection: Literal["no", "yes", "no_internet_service"]
+    techsupport: Literal["no", "yes", "no_internet_service"]
+    streamingtv: Literal["no", "yes", "no_internet_service"]
+    streamingmovies: Literal["no", "yes", "no_internet_service"]
+    contract: Literal["month-to-month", "one_year", "two_year"]
+    paperlessbilling: Literal["yes", "no"]
+    paymentmethod: Literal[
+        "electronic_check",
+        "mailed_check",
+        "bank_transfer_(automatic)",
+        "credit_card_(automatic)",
+    ]
+    tenure: int = Field(..., ge=0)
+    monthlycharges: float = Field(..., ge=0.0)
+    totalcharges: float = Field(..., ge=0.0)
+    
+class PredictResponse(BaseModel):
+    churn_probability: float
+    churn: bool
 
 app=FastAPI(title='Churn-Prediction')
 
